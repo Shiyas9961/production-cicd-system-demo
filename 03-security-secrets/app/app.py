a009c74270808881cd652d6e4c5ac97ab5032d7e
Shiyas9961 <shiyasshiyyu9961@gmail.com>
2026-06-27 16:08:39 +0530

    feat: remove ssecret from response

--- 03-security-secrets/app/app.py
+++ 03-security-secrets/app/app.py
@@ -9,7 +9,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return f"API KEY: {os.getenv('API_KEY')}"
-
+    return "Application running with secrets management"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
